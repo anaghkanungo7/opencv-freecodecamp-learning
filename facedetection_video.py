9ede0a5f69b56a1efd6ee42183a674894a948ad2
Anagh Kanungo <23740251+anaghkanungo7@users.noreply.github.com>
2022-01-12 14:47:23 +0530

    Update facedetection_video.py

--- facedetection_video.py
+++ facedetection_video.py
@@ -7,7 +7,7 @@ while True:
     haar_cascade = cv.CascadeClassifier('./haar_face.xml')
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-    faces_rect = haar_cascade.detectMultiScale(gray, 1.1, 8)
+    faces_rect = haar_cascade.detectMultiScale(gray, 1.1, 6)
     print("Number of faces found: " + str(len(faces_rect)))
     for (x, y, w, h) in faces_rect:
         cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
